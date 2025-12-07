e446bf1519678ee71af6b5bfb25db83f6fc0d0e7
zhengtiantian <xiz00188@gmail.com>
2025-12-07 22:07:55 +0000

    add workflow list

---
+++ src/main/java/org/example/quantapi/controller/ScriptController.java
@@ -0,0 +1,35 @@
+package org.example.quantapi.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api")
+public class ScriptController {
+
+    private static final String QUANT_DATA_BASE = "http://quant_data:8000";
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/scripts")
+    public ResponseEntity<String> listScripts() {
+        String url = QUANT_DATA_BASE + "/scripts";
+        String response = restTemplate.getForObject(url, String.class);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/run/{path}")
+    public ResponseEntity<String> runScript(@PathVariable String path) {
+        String url = QUANT_DATA_BASE + "/run/" + path;
+        String response = restTemplate.getForObject(url, String.class);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/stop/{path}")
+    public ResponseEntity<String> stopScript(@PathVariable String path) {
+        String url = QUANT_DATA_BASE + "/stop/" + path;
+        String response = restTemplate.postForObject(url, null, String.class);
+        return ResponseEntity.ok(response);
+    }
+}
